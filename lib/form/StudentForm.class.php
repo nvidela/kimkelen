f54ddabeacf8c64068eed9493cbbbf9e336f00af
Naimid Videla <nai.videla@gmail.com>
2018-02-06 09:22:21 -0300

    modifico form en bba,lvm y agrego traducciones

--- lib/form/StudentForm.class.php
+++ lib/form/StudentForm.class.php
@@ -109,7 +109,7 @@ class StudentForm extends BaseStudentForm
 
   public function getFormFieldsDisplay()
   {
-    $personal_data_fields = array('person-lastname', 'person-firstname', 'person-identification_type', 'person-identification_number', 'person-sex', 'global_file_number', 'origin_school_id', 'person-cuil', 'person-birthdate', 'person-birth_country', 'person-birth_state','person-birth_department', 'person-birth_city', 'person-photo', 'person-observations');
+    $personal_data_fields = array('person-lastname', 'person-firstname', 'person-identification_type', 'person-identification_number', 'person-sex', 'global_file_number', 'origin_school_id', 'person-cuil', 'person-birthdate', 'person-birth_country', 'person-birth_state','person-birth_department', 'person-birth_city', 'person-photo');
 
     if($this->getObject()->getPerson()->getPhoto())
     {
@@ -128,7 +128,7 @@ class StudentForm extends BaseStudentForm
         $personal_data_fields = array_merge($personal_data_fields, array('current_file_data', 'delete_file_data'));
         $this->getWidgetSchema()->moveField('file_data', sfWidgetFormSchema::LAST);
     }
-    
+    $personal_data_fields[] = 'person-observations';
     return array(
           'Personal data'   =>  $personal_data_fields,
           'Contact data'    =>  array('person-email', 'person-phone', 'person-address'),
